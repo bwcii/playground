c6da93fbb2ec13afab384eea1ab38005782174c6
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-01-04 14:04:08 +0000

    First card in progress

--- src/app/reverse-text/reverse-text.component.ts
+++ src/app/reverse-text/reverse-text.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-reverse-text',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatFormFieldModule,
+    CommonModule,
   ],
   templateUrl: './reverse-text.component.html',
   styleUrl: './reverse-text.component.css'
 })
 export class ReverseTextComponent {
-
+  title = "Reverse Text"
+  enabledState: boolean = false;
 }
