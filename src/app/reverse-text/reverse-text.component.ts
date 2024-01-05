80bac0547cc6cdbb4fda173d9ef3684b2f1cace5
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-01-05 15:58:20 +0000

    Updated the component ui and reverse functionality

--- src/app/reverse-text/reverse-text.component.ts
+++ src/app/reverse-text/reverse-text.component.ts
@@ -3,7 +3,7 @@ import { MatCardModule } from '@angular/material/card'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input';
@@ -20,7 +20,8 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatIconModule,
     // Thanks JSONDerulo via https://stackoverflow.com/questions/77759649/why-is-my-mat-slide-toggle-state-changing-but-the-animations-wont-work-unless
-    MatInputModule
+    MatInputModule,
+    CommonModule,
   ],
   templateUrl: './reverse-text.component.html',
   styleUrl: './reverse-text.component.css'
@@ -36,5 +37,12 @@ export class ReverseTextComponent {
     var mixed = splitstring.reverse();
     var unmixed = mixed.join("");
     this.reversedText = unmixed;
+  }
+
+  checkFunction() {
+    this.checked = !this.checked;
+    if (!this.checked) {
+      this.reversedText = "";
+    }
   }
 }
