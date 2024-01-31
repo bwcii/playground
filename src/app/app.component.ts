14430f015ae56cc6c0a970ea73db408d54b3099c
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-01-31 04:43:11 +0000

    Formatted repo for firebase hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,28 @@ import { RandomCatComponent } from './random-cat/random-cat.component';
 import { IpInfoComponent } from './ip-info/ip-info.component';
 import { IpGeoInfoComponent } from './ip-geo-info/ip-geo-info.component';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1ZemmksnwuxZA_yHXGAsUEWwYkyvSDOg",
+  authDomain: "angularplayground-410317.firebaseapp.com",
+  projectId: "angularplayground-410317",
+  storageBucket: "angularplayground-410317.appspot.com",
+  messagingSenderId: "443072296089",
+  appId: "1:443072296089:web:96a0c73a64130a11f03408",
+  measurementId: "G-837XB1R1RX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 @Component({
   selector: 'app-root',
   standalone: true,
